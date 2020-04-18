e02c94402d4babf1605071c9dbdfc916c91af254
sarthakgi <63248015+sarthakgi@users.noreply.github.com>
2020-04-18 20:52:19 +0530

    Create class_sweets.py

---
+++ class_sweets.py
@@ -0,0 +1,14 @@
+class Festival:
+    pass
+
+sweet1 = Festival()
+sweet1.name = 'Gujiya'
+sweet1.price = 20
+print(sweet1.name)
+print(sweet1.price,"\n")
+
+sweet2 = Festival()
+sweet2.name = 'Laddoo'
+sweet2.price = 15
+print(sweet2.name)
+print(sweet2.price)

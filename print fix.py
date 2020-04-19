b8fd3327ed4a10ab4c3696077e2794034b70004f
sarthakgi <63248015+sarthakgi@users.noreply.github.com>
2020-04-19 19:52:55 +0530

    Create print fix.py

---
+++ print fix.py
@@ -0,0 +1,11 @@
+print('Why won\'t this line of code print')
+
+# print the message
+print('This line fails too!')
+
+# print the message
+print ("I think I know how to fix this one")
+
+# print the name entered by the user
+name=input('Please tell me your name: ')
+print(name)
